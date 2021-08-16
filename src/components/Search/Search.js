fbd63c6e817d721e5dd26981682e4e5421eb90c7
cllcnkrt <cllcnkrt@outlook.com>
2021-08-16 14:37:58 +0300

    search section added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import './Search.css'
+import React, { useState } from "react";
+import "./Search.css";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import PeopleIcon from '@material-ui/icons/People';
+import { Button } from "@material-ui/core";
 function Search() {
-    return (
-        <div className="search">
-            <h1>Date picker</h1>
-        </div>
-    )
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>
+          Number of guests
+          <PeopleIcon/>
+      </h2>
+      <input type="number" min={0} default value={2}/>
+      <Button>Search Airbnb</Button>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
