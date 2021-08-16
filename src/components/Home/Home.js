a363b9a4487c91fb7add518ad3955b491d991abe
cllcnkrt <cllcnkrt@outlook.com>
2021-08-16 11:56:34 +0300

    banner component and banner.css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 import './Home.css'
 function Home() {
     return (
         <div className="home">
-         
+            <h1>Home</h1>
+         <Banner/>
         </div>
     )
 }
