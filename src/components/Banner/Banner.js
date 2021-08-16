0c5fbaccc5830356ccc9284044d65de1a0025490
cllcnkrt <cllcnkrt@outlook.com>
2021-08-16 12:24:41 +0300

    date picker added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,21 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React,{useState} from "react";
+import Search from "../Search/Search";
 import "./Banner.css";
 
 function Banner() {
+
+   const [showSearch, setShowSearch] = useState(true)
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search/>}
+
+        <Button variant="outlined" className="banner__searchButton" onClick={() => setShowSearch(!showSearch)}>
+         {showSearch ?"Hide": "Search Dates"}
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Search.css'
+function Search() {
+    return (
+        <div className="search">
+            <h1>Date picker</h1>
+        </div>
+    )
+}
+
+export default Search
